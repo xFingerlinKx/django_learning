37b5f47aaad8cf99efe94a42028c91ba1eaf3c96
Artur Apanasov <aaa@admport.by>
2021-04-10 11:17:32 +0300

    admin interface added for model

--- django_3_book/samplesite/bboard/admin.py
+++ django_3_book/samplesite/bboard/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import Bb
 
-admin.site.register(Bb)
+
+class BbAdmin(admin.ModelAdmin):
+    """ Класс редактора модели объявлений """
+    # последовательность имен полей, которые должны выводиться в списке записей
+    list_display = ('title', 'content', 'price', 'published')
+    # последовательность имен полей, которые должны быть преобразованы в гиперссылки, ведущие на страницу правки записи
+    list_display_links = ('title', 'content')
+    # последовательность имен полей, по которым должна выпол няться фильтрация
+    search_fields = ('title', 'content')
+
+
+admin.site.register(Bb, BbAdmin)
+
