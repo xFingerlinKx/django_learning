1bae71c33501a855824363558916dd8fe3332541
Artur Apanasov <aaa@admport.by>
2021-05-14 17:34:02 +0300

    [ADD] Добавлены контроллеры смены/сброса пароля.

--- django_3_book/samplesite/samplesite/urls.py
+++ django_3_book/samplesite/samplesite/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 
 """
 Path в качестве параметров принимает строку с шаблонным путем и ссылку на контроллер-функцию
@@ -44,4 +53,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('accounts/password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('accounts/password_reset/', PasswordResetView.as_view(
+        subject_template_name='registration/reset_subject.txt',
+        email_template_name='registration/reset_email.txt'), name='password_reset'),
+    path('accounts/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path ('accounts/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(
+        template_name='registration/confirm_password.html'),
+          name='password__reset_confirm'),
+    path('accounts/reset/done/', PasswordResetCompleteView.as_view(
+        template_name='registration/password_confirmed.html'),
+         name='password_reset_complete'),
 ]
